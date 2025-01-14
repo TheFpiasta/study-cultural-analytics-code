9720f3c1b2f9d8d204f3b42b9dc5748a83a2f9c9
Felix Piasta <felix.piasta@studserv.uni-leipzig.de>
2025-01-14 18:08:44 +0100

    add save scraped response data to db

--- django-app/scraper/apis.py
+++ django-app/scraper/apis.py
@@ -1,39 +1,68 @@
 import json
+import re
 import traceback
 from datetime import datetime, date, timedelta
-from time import sleep
 import requests
 
 from django.http import StreamingHttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from scraper.models import ScraperRun, ScrapeBatch, ScrapeData
+
 
 @csrf_exempt
 def start(request):
+    req_data = []
+
+    if request.method == "GET":
+        req_data = request.args
+
     if request.method == "POST":
-        try:
-            data = json.loads(request.body.decode("utf-8"))
+        req_data = json.loads(request.body.decode("utf-8"))
 
-            name = data.get("name", "")
-            profile_id = data.get("profile_id", 0)
-            start_cursor = data.get("start_cursor", "")  # optional
-            scrape_to_date = data.get("scrape_to_date", "01.01.2025-0:0:0")  # optional
-            scrape_max_notes = data.get("scrape_max_notes", 99999)  # optional
-            scrape_max_batches = data.get("scrape_max_batches", 200)  # optional
-            scrape_notes_per_batch = data.get("scrape_notes_per_batch", 10)  # optional
+    try:
+        name = req_data.get("name", "")
+        profile_id = req_data.get("profile_id", 0)
+        start_cursor = req_data.get("start_cursor", "")  # optional
+        scrape_to_date = req_data.get("scrape_to_date", "01.01.2025-0:0:0")  # optional
+        scrape_max_notes = req_data.get("scrape_max_notes", 99999)  # optional
+        scrape_max_batches = req_data.get("scrape_max_batches", 200)  # optional
+        scrape_notes_per_batch = req_data.get("scrape_notes_per_batch", 10)  # optional
 
-            if name == "":
-                return JsonResponse({"error": "name is required"}, status=400)
+        if name == "":
+            return JsonResponse({"error": "name is required"}, status=400)
 
-            if profile_id == "":
-                return JsonResponse({"error": "profile_id is required"}, status=400)
+        if profile_id == "":
+            return JsonResponse({"error": "profile_id is required"}, status=400)
 
-            scrape_to_date = datetime.strptime(scrape_to_date, "%d.%m.%Y-%H:%M:%S")
+        scrape_to_date = datetime.strptime(scrape_to_date, "%d.%m.%Y-%H:%M:%S")
 
-            query_id = 17888483320059182
+        query_id = 17888483320059182
 
-            def stream_output():
+        curr_run = ScraperRun(name=name,
+                              profile_id=profile_id,
+                              start_cursor=start_cursor,
+                              scrape_max_date=scrape_to_date,
+                              scrape_max_notes=scrape_max_notes,
+                              scrape_max_batches=scrape_max_batches,
+                              scrape_notes_per_batch=scrape_notes_per_batch,
+                              status="created",
+                              total_data_count=0)
+        curr_run.save()
+
+        def stream_output():
+            try:
                 next_cursor = start_cursor
+
+                scraped_bates = 0
+                scraped_notes = 0
+                date_in_range = True
+
+                dir_ext = re.sub(r'[^a-zA-Z0-9\-]', '', name.lower().replace(" ", "-"))
+                img_dir = f"{int(datetime.now().timestamp())}-{dir_ext}"
+
+                # todo mkdir img_dir
+                # todo refactor note to node
 
                 def get_graphql_url(cursor=""):
                     after = ""
@@ -41,44 +70,80 @@ def start(request):
                         after = f',"after":"{cursor}"'
                     return f'https://www.instagram.com/graphql/query/?query_id={query_id}&variables={{"id":"{profile_id}","first":{scrape_notes_per_batch}{after}}}'
 
+                def update_run_started(total_data_count):
+                    curr_run.status = "running"
+                    curr_run.total_data_count = total_data_count
+                    curr_run.save()
+
+                def save_img(img_url):
+                    # todo add column "img_name" to ScrapeData
+                    # todo add column "img_dir" to ScrapeBatch
+                    # todo downloade and save img in db and dir
+
+                    return None
+
+                def process_node(node, scrape_batch_id):
+                    img_url = node["display_url"]
+                    curr_node = ScrapeData(scraper_run_id=curr_run.id,
+                                           scrape_batch_id=scrape_batch_id,
+                                           node_id=node["id"],
+                                           type=node["__typename"],
+                                           text=node["edge_media_to_caption"]["edges"][0]["node"]["text"],
+                                           shortcode=node["shortcode"],
+                                           comment_count=node["edge_media_to_comment"]["count"],
+                                           comments_disabled=node["comments_disabled"],
+                                           taken_at_timestamp=node["taken_at_timestamp"],
+                                           display_height=node["dimensions"]["height"],
+                                           display_width=node["dimensions"]["width"],
+                                           display_url=img_url,
+                                           likes_count=node["edge_media_preview_like"]["count"],
+                                           owner_id=node["owner"]["id"],
+                                           thumbnail_url=node["thumbnail_url"],
+                                           thumbnail_resources=json.dumps(node["thumbnail_resources"]),
+                                           is_video=node["is_video"], )
+
+                    curr_node.save()
+                    save_img(img_url)
+
                 def process_batch():
                     graphql_url = get_graphql_url(next_cursor)
 
                     insta_response = requests.get(graphql_url)
                     insta_data = insta_response.json()
+                    insta_data_media = insta_data["data"]["user"]["edge_owner_to_timeline_media"]
 
-                    # todo save insta_data to database
-                    # todo downloade image
+                    if scraped_bates == 0:
+                        update_run_started(insta_data_media["count"])
 
-                    print(insta_data)
+                    curr_batch = ScrapeBatch(scraper_run_id=curr_run.id,
+                                             notes_in_batch=len(insta_data_media["edges"]),
+                                             has_next_page=insta_data_media["page_info"]["has_next_page"],
+                                             end_cursor=insta_data_media["page_info"]["end_cursor"],
+                                             status=insta_data["status"],
+                                             extensions=json.dumps(insta_data["extensions"]), )
+                    curr_batch.save()
 
-                    note_count = 1
-                    return {"note_count":note_count, "last_date":datetime.strptime("12.01.2025", "%d.%m.%Y")}
+                    for edge in insta_data_media["edges"]:
+                        process_node(edge["node"], curr_batch.id)
 
-                scraped_bates = 0
-                scraped_notes = 0
-                date_in_range = True
+                    return len(insta_data_media["edges"])
 
-                try:
-                    while scraped_bates < scrape_max_batches and scraped_notes < scrape_max_notes and date_in_range:
-                        result = process_batch()
-                        scraped_notes += result["note_count"]
-                        scraped_bates += 1
+                while scraped_bates < scrape_max_batches and scraped_notes < scrape_max_notes:
+                    scraped_notes += process_batch()
+                    scraped_bates += 1
 
-                        date_in_range = scrape_to_date <= result["last_date"]
+                    print(
+                        f"scraped_bates:{scraped_bates}, scraped_notes:{scraped_notes}")
+                    yield json.dumps({
+                        "scraped_bates": scraped_bates,
+                        "scraped_notes": scraped_notes
+                    }) + "\n"
 
-                        print(
-                            f"scraped_bates:{scraped_bates}, scraped_notes:{scraped_notes}, date_in_range:{date_in_range}")
-                        yield json.dumps({
-                            "scraped_bates": scraped_bates,
-                            "scraped_notes": scraped_notes,
-                            "date_in_range": date_in_range
-                        }) + "\n"
-                except Exception as err:
-                    yield json.dumps({"error": str(err) + " " + str(traceback.print_exc())}) + "\n"
+            except Exception as err:
+                yield json.dumps({"error": str(err) + " " + str(traceback.print_exc())}) + "\n"
 
-            response = StreamingHttpResponse(stream_output(), content_type="application/json")
-            return response
+        response = StreamingHttpResponse(stream_output(), content_type="application/json")
+        return response
 
-        except Exception as e:
-            return JsonResponse({"error": str(e) + " " + str(traceback.print_exc())}, status=500)
+    except Exception as e:
+        return JsonResponse({"error": str(e) + " " + str(traceback.print_exc())}, status=500)
